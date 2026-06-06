96a4b4454e7ab69213ad2a6c7a26f887568f5eba
Moriel Mauni <morielmauni@gmail.com>
2026-06-06 22:10:45 +0000

    Using NextJS Image component

--- app/photos/page.js
+++ app/photos/page.js
@@ -1,12 +1,21 @@
+import Image from "next/image";
+import dog1 from "@/public/images/dog1.png";
+
 export default function PhotosPage() {
   return (
     <>
       <div>
         <h1 className="text-2xl mb-8 font-semibold">Photos</h1>
 
-        <div className="grid grid-cols-2 gap-4 overflow-hidden">
-          <div className="h-60">
-            <img src="/images/dog1.png" className="object-cover w-full h-full"></img>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 overflow-hidden">
+          <div className="relative h-60">
+            <Image
+              fill
+              src={dog1}
+              className="object-cover w-full h-full"
+              alt="Dog-1"
+              sizes="(max-width: 768px) 100vw, 50vw"
+            />
           </div>
         </div>
       </div>
