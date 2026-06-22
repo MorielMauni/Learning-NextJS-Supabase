da40bf61bd6d034cdf5fe021b3531c6b98896c70
Moriel Mauni <morielmauni@gmail.com>
2026-06-22 19:11:35 +0000

    Adding tags to every blog post

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,30 +1,40 @@
-import { notFound } from 'next/navigation'
-import { getPost as getPostNotCached } from '@/lib/posts'
-import { cache } from 'react'
+import { notFound } from "next/navigation";
+import { getPost as getPostNotCached } from "@/lib/posts";
+import { cache } from "react";
+import Link from "next/link";
 
-const getPost = cache(
-  async (slug) => await getPostNotCached(slug)
-)
+const getPost = cache(async (slug) => await getPostNotCached(slug));
 
-export async function generateMetadata({ params }, parent) {
+export async function generateMetadata({ params }) {
   try {
-    const { frontmatter } = await getPost(params.slug)
-    return frontmatter
-  } catch (e) { }
+    const { frontmatter } = await getPost(params.slug);
+    return frontmatter;
+  } catch (e) {}
 }
 
 export default async function BlogPage({ params }) {
-  let post
+  let post;
 
   try {
-    post = await getPost(params.slug)
+    post = await getPost(params.slug);
   } catch (e) {
-    notFound()
+    notFound();
   }
 
   return (
     <article className="prose dark:prose-invert">
+      <div className="flex space-x-2 mb-8">
+        {post.frontmatter.tags.map((tag) => (
+          <Link
+            key={tag}
+            href={`/blog/?tags=${tag}`}
+            className="dark:text-gray-400 text-gray-500"
+          >
+            {tag}
+          </Link>
+        ))}
+      </div>
       {post.content}
     </article>
-  )
+  );
 }
